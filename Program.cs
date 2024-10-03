71475d8e11711ea6b71f36340999ccde4cade68f
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2024-10-03 17:16:55 +0100

    Add packages and basic scaffolding for identity

--- Program.cs
+++ Program.cs
@@ -1,10 +1,76 @@
 using CSIDE.Components;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+    .AddInteractiveServerComponents()
+    .AddMicrosoftIdentityConsentHandler();
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(options =>
+                {
+                    builder.Configuration.Bind("AzureAd", options);
+                    options.Events = new OpenIdConnectEvents
+                    {
+                        OnRedirectToIdentityProvider = async ctxt =>
+                        {
+                            // Invoked before redirecting to the identity provider to authenticate. 
+                            // This can be used to set ProtocolMessage.State
+                            // that will be persisted through the authentication process. 
+                            // The ProtocolMessage can also be used to add or customize
+                            // parameters sent to the identity provider.
+                            await Task.Yield();
+                        },
+                        OnAuthenticationFailed = async ctxt =>
+                        {
+                            // They tried to log in but it failed
+                            await Task.Yield();
+                        },
+                        OnSignedOutCallbackRedirect = async ctxt =>
+                        {
+                            ctxt.HttpContext.Response.Redirect(ctxt.Options.SignedOutRedirectUri);
+                            ctxt.HandleResponse();
+                            await Task.Yield();
+                        },
+                        OnTicketReceived = async ctxt =>
+                        {
+                            if (ctxt.Principal != null)
+                            {
+                                if (ctxt.Principal.Identity is ClaimsIdentity identity)
+                                {
+                                    var colClaims = ctxt.Principal.Claims.ToList();
+                                    var IdentityProvider = colClaims.FirstOrDefault(
+                                        c => c.Type == "http://schemas.microsoft.com/identity/claims/identityprovider")?.Value;
+                                    var Objectidentifier = colClaims.FirstOrDefault(
+                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+                                    var EmailAddress = colClaims.FirstOrDefault(
+                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
+                                    var FirstName = colClaims.FirstOrDefault(
+                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")?.Value;
+                                    var LastName = colClaims.FirstOrDefault(
+                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname")?.Value;
+                                    var AzureB2CFlow = colClaims.FirstOrDefault(
+                                        c => c.Type == "http://schemas.microsoft.com/claims/authnclassreference")?.Value;
+                                    var auth_time = colClaims.FirstOrDefault(
+                                        c => c.Type == "auth_time")?.Value;
+                                    var DisplayName = colClaims.FirstOrDefault(
+                                        c => c.Type == "name")?.Value;
+                                    var idp_access_token = colClaims.FirstOrDefault(
+                                        c => c.Type == "idp_access_token")?.Value;
+                                }
+                            }
+                            await Task.Yield();
+                        },
+                    };
+                });
+builder.Services.AddControllersWithViews()
+    .AddMicrosoftIdentityUI();
 
 var app = builder.Build();
 
@@ -17,6 +83,9 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
