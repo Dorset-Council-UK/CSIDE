ae1d94a745f4234aef0bc3ec352af3bc30b18943
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2024-10-07 17:09:50 +0100

    WIP: Add policy, access denied page and claims transformer

---
+++ Authorization/ClaimsTransformer.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.Data;
+using System.Security.Claims;
+
+namespace CSIDE.Authorization
+{
+    public class ClaimsTransformer() : IClaimsTransformation
+    {
+
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
+        {
+            var claimsIdentity = (ClaimsIdentity)principal.Identity;
+            var userIdClaim = claimsIdentity.FindFirst(System.Security.Claims.ClaimTypes.NameIdentifier);
+            var userId = userIdClaim.Value;
+
+            //DEBUGGING PURPOSES ONLY
+            Claim customRoleClaim = new(claimsIdentity.RoleClaimType, "Administrator");
+            claimsIdentity.AddClaim(customRoleClaim);
+            //
+
+            //fetch roles from user roles table
+            //var roles = _commonRepository.GetUserRoles(userId);
+
+            //foreach (var role in roles)
+            //{
+            //    Claim customRoleClaim = new(claimsIdentity.RoleClaimType, role.Role.RoleName);
+            //    claimsIdentity.AddClaim(customRoleClaim);
+            //}
+            //fetch roles from extension_roles claim
+            //Removed as not currently needed, but may be added/changed in future
+            //var extensionRolesClaim = claimsIdentity.FindFirst("extension_roles");
+            //if (extensionRolesClaim != null)
+            //{
+            //    /*TODO - Check for null*/
+            //    var extensionRolesStr = extensionRolesClaim.Value;
+            //    string[] extensionRolesList = extensionRolesStr.Split(";");
+            //    foreach(string extensionRole in extensionRolesList)
+            //    {
+            //        Claim customRoleClaim = new Claim(claimsIdentity.RoleClaimType, extensionRole);
+            //        claimsIdentity.AddClaim(customRoleClaim);
+            //    }
+            //}
+
+            return Task.FromResult(principal);
+        }
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
+using CSIDE.Authorization;
 using CSIDE.Components;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
@@ -40,37 +42,15 @@ builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
                         },
                         OnTicketReceived = async ctxt =>
                         {
-                            if (ctxt.Principal != null)
-                            {
-                                if (ctxt.Principal.Identity is ClaimsIdentity identity)
-                                {
-                                    var colClaims = ctxt.Principal.Claims.ToList();
-                                    var IdentityProvider = colClaims.FirstOrDefault(
-                                        c => c.Type == "http://schemas.microsoft.com/identity/claims/identityprovider")?.Value;
-                                    var Objectidentifier = colClaims.FirstOrDefault(
-                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
-                                    var EmailAddress = colClaims.FirstOrDefault(
-                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress")?.Value;
-                                    var FirstName = colClaims.FirstOrDefault(
-                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname")?.Value;
-                                    var LastName = colClaims.FirstOrDefault(
-                                        c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname")?.Value;
-                                    var AzureB2CFlow = colClaims.FirstOrDefault(
-                                        c => c.Type == "http://schemas.microsoft.com/claims/authnclassreference")?.Value;
-                                    var auth_time = colClaims.FirstOrDefault(
-                                        c => c.Type == "auth_time")?.Value;
-                                    var DisplayName = colClaims.FirstOrDefault(
-                                        c => c.Type == "name")?.Value;
-                                    var idp_access_token = colClaims.FirstOrDefault(
-                                        c => c.Type == "idp_access_token")?.Value;
-                                }
-                            }
                             await Task.Yield();
                         },
                     };
                 });
-builder.Services.AddControllersWithViews()
-    .AddMicrosoftIdentityUI();
+
+builder.Services.AddTransient<IClaimsTransformation, ClaimsTransformer>();
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("CanAccessApp", policy => policy.RequireRole("Administrator"));
 
 var app = builder.Build();
 
@@ -85,7 +65,6 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
 app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
