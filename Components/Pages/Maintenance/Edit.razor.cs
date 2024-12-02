9ceb19249989e3ab4462aef23664df95723ea04d
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2024-12-02 12:04:50 +0000

    Convert pages to use code behind where appropriate

---
+++ Components/Pages/Maintenance/Edit.razor.cs
@@ -0,0 +1,208 @@
+﻿using BlazorBootstrap;
+using CSIDE.Components.Maintenance;
+using CSIDE.Components.Mapping;
+using CSIDE.Data.Models.Maintenance;
+using CSIDE.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using NetTopologySuite.IO;
+using NetTopologySuite.Features;
+using FluentValidation;
+
+namespace CSIDE.Components.Pages.Maintenance
+{
+    public partial class Edit(IDbContextFactory<ApplicationDbContext> contextFactory, NavigationManager navigationManager)
+    {
+        [Parameter]
+        public int JobId { get; set; }
+
+        private Job? Job { get; set; }
+        private JobStatus[]? JobStatuses { get; set; }
+        private JobPriority[]? JobPriorities { get; set; }
+        private Team[]? MaintenanceTeams { get; set; }
+        private ProblemType[]? ProblemTypes { get; set; }
+
+        private JobEditForm? childJobEditForm;
+        private List<int> SelectedProblemTypes { get; set; } = [];
+
+        private List<BreadcrumbItem>? NavItems;
+        private Modal routeValidationModal = default!;
+        private Modal errorModal = default!;
+
+        private EditMap? editMap;
+        private bool IsBusy { get; set; }
+        private string? ErrorMessage { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            NavItems =
+            [
+                new BreadcrumbItem{ Text = localizer["Home Title"], Href ="/" },
+                new BreadcrumbItem{ Text = localizer["Maintenance Title"], Href="/Maintenance" },
+                new BreadcrumbItem{ Text = localizer["Maintenance Edit Title", JobId], IsCurrentPage = true }
+            ];
+            IsBusy = true;
+            try
+            {
+                using var context = contextFactory.CreateDbContext();
+                JobStatuses = await context.MaintenanceJobStatuses.AsNoTracking().OrderBy(s => s.SortOrder).ToArrayAsync();
+                JobPriorities = await context.MaintenanceJobPriorities.AsNoTracking().OrderBy(p => p.SortOrder).ToArrayAsync();
+                MaintenanceTeams = await context.MaintenanceTeams.AsNoTracking().OrderBy(p => p.Name).ToArrayAsync();
+                ProblemTypes = await context.ProblemTypes.AsNoTracking().OrderBy(p => p.Name).ToArrayAsync();
+                Job = await context.MaintenanceJobs.IgnoreAutoIncludes().Include(j => j.ProblemTypes).FirstOrDefaultAsync(j => j.Id == JobId);
+                SelectedProblemTypes = Job!.ProblemTypes.Select(j => j.ProblemTypeId).ToList();
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        private async Task SubmitFormAsync()
+        {
+            if (Job is null || IsBusy)
+            {
+                ErrorMessage = null;
+                return;
+            }
+            if (await childJobEditForm!.ValidateAsync())
+            {
+                IsBusy = true;
+                StateHasChanged();
+                try
+                {
+                    if (Job is not null)
+                    {
+                        using var context = contextFactory.CreateDbContext();
+                        context.Update(Job);
+                        await UpdateMaintenanceProblemTypes(SelectedProblemTypes, context);
+                        await context.SaveChangesAsync();
+                        //redirect
+                        NavigateBackToJobDetailsPage();
+                    }
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    ErrorMessage = localizer["Concurrency Error Message", localizer["Maintenance Details Title", Job.Id]];
+                }
+                catch (Exception ex)
+                {
+                    ErrorMessage = localizer["Save Error Message"];
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+        }
+
+        private async Task UpdateMaintenanceProblemTypes(List<int> selectedProblemTypes, ApplicationDbContext context)
+        {
+            if (Job is null) return;
+            if (selectedProblemTypes == null)
+            {
+                await context.MaintenanceJobProblemTypes.Where(c => c.JobId == Job.Id).ExecuteDeleteAsync();
+                return;
+            }
+
+            //delete problems not needed anymore
+            await context.MaintenanceJobProblemTypes.Where(c => c.JobId == Job.Id && !selectedProblemTypes.Contains(c.ProblemTypeId)).ExecuteDeleteAsync();
+
+            //add new problem types
+            foreach (int problemType in selectedProblemTypes)
+            {
+                if (!context.MaintenanceJobProblemTypes.Where(c => c.JobId == Job.Id && c.ProblemTypeId == problemType).Any())
+                {
+                    context.MaintenanceJobProblemTypes.Add(new JobProblemType { ProblemTypeId = problemType, JobId = Job.Id });
+                }
+            }
+            return;
+        }
+
+        private void NavigateBackToJobDetailsPage()
+        {
+            navigationManager.NavigateTo($"/Maintenance/Details/{JobId}");
+        }
+
+        //TODO - The following is reused in Edit.razor, so should be shared somewhere (unless they diverge significantly)
+
+        private async Task ValidateGeometry(string features)
+        {
+            //check the geometry is a single point and is on a valid route
+            GeoJsonReader _geoJsonReader = new();
+            FeatureCollection featureCollection = _geoJsonReader.Read<FeatureCollection>(features);
+
+            CSIDE.Validators.Geometry.GeometryValidator validator = new(contextFactory, localizer);
+
+            var result = await validator.ValidateAsync(featureCollection, options => options.IncludeRuleSets("Single Point", "Point On Route"));
+            if (result.IsValid)
+            {
+                //get route code and update Job.Geom
+                if (Job is not null)
+                {
+                    Job.Geom = featureCollection.First().Geometry.Centroid;
+                    Job.Geom.SRID = 27700;
+                    using var context = contextFactory.CreateDbContext();
+                    //TODO - Move this to shared location
+                    var Route = await context.Routes.Where(r => r.Geom.Distance(Job.Geom) < 10).OrderBy(r => r.Geom.Distance(Job.Geom)).FirstOrDefaultAsync();
+                    if (Route is not null)
+                    {
+                        Job.RouteId = Route.RouteCode;
+                    }
+                }
+            }
+            else
+            {
+                // Check to see what the error is and show appropriate error message
+                // first check if the geometry was invalid
+                if (result.Errors.Any(failure => failure.ErrorCode == "GEOM_OUTSIDE_BOUNDS") || result.Errors.Any(failure => failure.ErrorCode == "INVALID_GEOM"))
+                {
+                    //show generic error
+                    await ShowGeometryValidationErrorModal();
+                }
+                // if its valid, check to see if there was no route nearby
+                // NOTE I know it seems backwards to test this way round, but if you don't,
+                // invalid geometries always come back saying 'no route found', which is not the right message
+                // TODO - Improve logic through use of conditional validation
+                else if (result.Errors.Any(failure => failure.ErrorCode == "NO_ROUTE_NEARBY"))
+                {
+                    //we assigned the geom at this point in case the user goes ahead with overriding the route ID
+                    if (Job is not null)
+                    {
+                        Job.Geom = featureCollection.First().Geometry.Centroid;
+                        Job.Geom.SRID = 27700;
+                    }
+                    await ShowRouteValidationModal();
+                }
+            }
+            StateHasChanged();
+        }
+
+        private async Task ShowRouteValidationModal()
+        {
+            await routeValidationModal.ShowAsync();
+        }
+
+        private async Task ShowGeometryValidationErrorModal()
+        {
+            await errorModal.ShowAsync();
+        }
+
+        private async Task HideRouteValidationModal()
+        {
+            await routeValidationModal.HideAsync();
+        }
+
+        private async Task ClearDrawnGeometries()
+        {
+            if (Job is not null)
+            {
+                Job.Geom = null;
+                Job.RouteId = null;
+            }
+            await editMap!.ClearDrawnGeometries();
+            await routeValidationModal.HideAsync();
+            await errorModal.HideAsync();
+        }
+    }
+}
---
+++ Components/Pages/Maintenance/Jobs.razor.cs
@@ -0,0 +1,108 @@
+﻿using BlazorBootstrap;
+using CSIDE.Data;
+using CSIDE.Data.Models.Maintenance;
+using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
+using NodaTime;
+
+namespace CSIDE.Components.Pages.Maintenance
+{
+    public partial class Jobs(IDbContextFactory<ApplicationDbContext> contextFactory)
+    {
+        private List<BreadcrumbItem>? NavItems;
+
+        [SupplyParameterFromQuery]
+        private string? RouteId { get; set; }
+        [SupplyParameterFromQuery]
+        private string[]? ParishId { get; set; }
+        [SupplyParameterFromQuery]
+        private string? LoggedById { get; set; }
+        [SupplyParameterFromQuery]
+        private string? AssignedToTeamId { get; set; }
+        [SupplyParameterFromQuery]
+        private string? JobPriorityId { get; set; }
+        [SupplyParameterFromQuery]
+        private string? JobStatusId { get; set; }
+        [SupplyParameterFromQuery]
+        private DateOnly? LogDateFrom { get; set; }
+        [SupplyParameterFromQuery]
+        private DateOnly? LogDateTo { get; set; }
+        [SupplyParameterFromQuery]
+        private DateOnly? CompletedDateFrom { get; set; }
+        [SupplyParameterFromQuery]
+        private DateOnly? CompletedDateTo { get; set; }
+
+        private List<Job>? SearchResults;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            NavItems =
+            [
+                new BreadcrumbItem{ Text = localizer["Home Title"], Href ="/" },
+                new BreadcrumbItem{ Text = localizer["Maintenance Title"], Href="/Maintenance" },
+                new BreadcrumbItem{ Text = localizer["Search Results Title"], IsCurrentPage = true }
+            ];
+            //fetch results
+            using var context = contextFactory.CreateDbContext();
+
+            var query = context.MaintenanceJobs.AsQueryable();
+
+            if (RouteId is not null)
+            {
+                query = query.Where(j => j.RouteId == RouteId);
+            }
+            if (ParishId is not null && ParishId.Length != 0)
+            {
+                var parsedParishIds = ParishId
+                    .Where(id => int.TryParse(id, out _))
+                    .Select(id => int.Parse(id))
+                    .ToList();
+                if (parsedParishIds.Count != 0)
+                {
+                    query = query.Where(j => j.ParishId != null && parsedParishIds.Contains(j.ParishId.Value));
+                }
+
+            }
+            if (AssignedToTeamId is not null && int.TryParse(AssignedToTeamId, out int parsedAssignedToTeamId))
+            {
+                query = query.Where(j => j.MaintenanceTeamId == parsedAssignedToTeamId);
+            }
+            if (JobPriorityId is not null && int.TryParse(JobPriorityId, out int parsedPriorityId))
+            {
+                query = query.Where(j => j.JobPriorityId == parsedPriorityId);
+            }
+            if (JobStatusId is not null && int.TryParse(JobStatusId, out int parsedStatusId))
+            {
+                query = query.Where(j => j.JobStatusId == parsedStatusId);
+            }
+            if (LogDateFrom is not null)
+            {
+                query = query.Where(j => j.LogDate >= ConvertDateToInstant(LogDateFrom.Value));
+            }
+            if (LogDateTo is not null)
+            {
+                query = query.Where(j => j.LogDate < ConvertDateToInstant(LogDateTo.Value).Plus(Duration.FromDays(1)));
+            }
+            if (CompletedDateFrom is not null)
+            {
+                query = query.Where(j => j.CompletionDate >= NodaTime.LocalDate.FromDateOnly(CompletedDateFrom.Value));
+            }
+
+            if (CompletedDateTo is not null)
+            {
+                query = query.Where(j => j.CompletionDate < NodaTime.LocalDate.FromDateOnly(CompletedDateTo.Value).PlusDays(1));
+            }
+
+            SearchResults = await query.OrderByDescending(j => j.LogDate).Take(500).ToListAsync();
+
+        }
+
+        private static Instant ConvertDateToInstant(DateOnly date)
+        {
+            var timezone = NodaTime.DateTimeZoneProviders.Tzdb.GetSystemDefault();
+            var localDate = NodaTime.LocalDate.FromDateOnly(date);
+            var zonedDate = localDate.AtStartOfDayInZone(timezone);
+            return zonedDate.ToInstant();
+        }
+    }
+}
