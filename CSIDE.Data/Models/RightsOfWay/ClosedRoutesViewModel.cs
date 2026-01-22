f8d3145900199d2c7fab00183cf43e32065901c7
Lucy Bishop <lucy.bishop@dorsetcouncil.gov.uk>
2026-01-22 13:11:50 +0000

    Persistent state added to rights of way closure component, view model created

---
+++ CSIDE.Data/Models/RightsOfWay/ClosedRoutesViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSIDE.Data.Models.RightsOfWay
+{
+    public class ClosedRoutesViewModel
+    {
+        public string? RouteCode { get; set; }
+        public DateOnly? ClosureEndDate { get; set; }
+    }
+}
