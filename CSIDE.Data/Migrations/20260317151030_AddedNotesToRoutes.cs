b371036b0600b0b644eaf8082827b5ae5755efd8
Lucy B <114388582+VulpesFerrilata@users.noreply.github.com>
2026-03-17 16:57:29 +0000

    Added notes field to Rights of Way (#11)

---
+++ CSIDE.Data/Migrations/20260317151030_AddedNotesToRoutes.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CSIDE.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedNotesToRoutes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "notes",
+                schema: "cside",
+                table: "routes",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "notes",
+                schema: "cside",
+                table: "routes");
+        }
+    }
+}
