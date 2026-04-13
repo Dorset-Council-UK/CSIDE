46b207e4afca9c823d492cd39416aab556940064
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2026-04-13 18:05:12 +0100

    Add IsActive bool to Legal Status lookup

---
+++ CSIDE.Data/EntitiesConfiguration/LegalStatusConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using CSIDE.Data.Models.RightsOfWay;
+
+namespace CSIDE.Data.EntitiesConfiguration
+{
+    internal sealed class LegalStatusConfiguration : IEntityTypeConfiguration<LegalStatus>
+    {
+        public void Configure(EntityTypeBuilder<LegalStatus> builder)
+        {
+            builder.Property(x => x.IsActive).HasDefaultValue(true);
+        }
+    }
+}
