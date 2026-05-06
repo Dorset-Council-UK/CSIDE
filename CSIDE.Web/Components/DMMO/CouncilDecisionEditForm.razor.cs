6f71d73dfa742b2ba4ec6565546d44bee7150b3b
Lucy Bishop <lucy.bishop@dorsetcouncil.gov.uk>
2026-05-06 14:08:47 +0100

    Added EditForm

---
+++ CSIDE.Web/Components/DMMO/CouncilDecisionEditForm.razor.cs
@@ -0,0 +1,73 @@
+﻿using Blazored.FluentValidation;
+using CSIDE.Data.Models.DMMO;
+using CSIDE.Data.Models.Shared;
+using Microsoft.AspNetCore.Components;
+using NodaTime;
+
+namespace CSIDE.Web.Components.DMMO
+{
+    public partial class CouncilDecisionEditForm
+    {
+        [Parameter, EditorRequired]
+        public DMMOCouncilDecision? CouncilDecision { get; set; }
+        [Parameter, EditorRequired]
+        public ICollection<CouncilDecisionType> CouncilDecisionTypes { get; set; }
+        [Parameter]
+        public bool IsBusy { get; set; }
+        [Parameter]
+        public bool IsEdit { get; set; }
+        [Parameter, EditorRequired]
+        public EventCallback OnSubmit { get; set; }
+        [Parameter, EditorRequired]
+        public EventCallback OnCancel { get; set; }
+
+        private FluentValidationValidator? fluentValidationValidator;
+
+        private async Task SubmitFormAsync()
+        {
+            if (OnSubmit.HasDelegate)
+            {
+                await OnSubmit.InvokeAsync();
+            }
+        }
+
+        public async Task<bool> ValidateAsync()
+        {
+            if (!IsEdit)
+            {
+                return await fluentValidationValidator!.ValidateAsync(opts => opts.IncludeAllRuleSets());
+            }
+            return await fluentValidationValidator!.ValidateAsync();
+        }
+
+        private void UpdateDateSealedProperty(ChangeEventArgs eventArgs)
+        {
+            UpdateDateProperty(eventArgs, date => CouncilDecision!.Date = date);
+        }
+
+        private void UpdateDateProperty(ChangeEventArgs eventArgs, Action<LocalDate?> updateProperty)
+        {
+            if (CouncilDecision is not null && eventArgs.Value is not null)
+            {
+                try
+                {
+                    var pattern = NodaTime.Text.LocalDatePattern.CreateWithInvariantCulture("yyyy-MM-dd");
+                    var parseResult = pattern.Parse(eventArgs.Value.ToString()!);
+                    updateProperty(parseResult.Value);
+                }
+                catch (Exception)
+                {
+                    // Problem parsing date, don't update
+                }
+            }
+        }
+
+        private async Task HandleCancel()
+        {
+            if (OnCancel.HasDelegate)
+            {
+                await OnCancel.InvokeAsync();
+            }
+        }
+    }
+}
