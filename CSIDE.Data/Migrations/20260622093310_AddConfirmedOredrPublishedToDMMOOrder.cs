8fede4f299998fa8c8382fbad86b59ad6513b814
Rob Quincey <rob.quincey@dorsetcouncil.gov.uk>
2026-06-22 11:15:00 +0100

    Update Order card and add migration

---
+++ CSIDE.Data/Migrations/20260622093310_AddConfirmedOredrPublishedToDMMOOrder.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NodaTime;
+
+#nullable disable
+
+namespace CSIDE.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfirmedOredrPublishedToDMMOOrder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<LocalDate>(
+                name: "confirmation_published_date",
+                schema: "cside",
+                table: "dmmo_orders",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "confirmation_published_date",
+                schema: "cside",
+                table: "dmmo_orders");
+        }
+    }
+}
