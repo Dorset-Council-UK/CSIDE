de87303d4f88b1cd336972c062920989e5b0366a
Lucy Bishop <lucy.bishop@dorsetcouncil.gov.uk>
2026-08-19 15:12:02 +0100

    Updated to use variable instead

--- CSIDE.Web/Components/Pages/RightsOfWay/Index.razor.cs
+++ CSIDE.Web/Components/Pages/RightsOfWay/Index.razor.cs
@@ -48,14 +48,15 @@ namespace CSIDE.Web.Components.Pages.RightsOfWay
         {
             if (RouteIDSearch is not null)
             {
+                var trimmedSearchTerm = RouteIDSearch.Trim();
                 IsBusy = true;
                 RouteIDSearchErrorMessage = null;
                 try
                 {
-                    var routeExists = await rightsOfWayService.RouteExists(RouteIDSearch.Trim());
+                    var routeExists = await rightsOfWayService.RouteExists(trimmedSearchTerm);
                     if (routeExists)
                     {
-                        navigationManager.NavigateTo($"rights-of-way/Details/{RouteIDSearch.Trim()}");
+                        navigationManager.NavigateTo($"rights-of-way/Details/{trimmedSearchTerm}");
                         return;
                     }
 
